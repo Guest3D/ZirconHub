192e76745370035d60deb0e7f44c342d69537430
Guest3D <86285226+Guest3D@users.noreply.github.com>
2025-01-21 12:40:02 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,10 @@
 -- go fishing 
 if game.PlaceId == 116605585218149 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Guest3D/ZirconHub/refs/heads/main/GoFishing.lua"))()
+-- Pillow Fight
+elseif game.PlaceId == 11747070874 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Guest3D/ZirconHub/refs/heads/main/PillowFight.lua"))()
+-- parkour ninja
+elseif game.PlaceId == 147848991 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Guest3D/ZirconHub/refs/heads/main/BeAParkourNinja.lua"))()
 end
